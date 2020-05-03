4d7243d53d6d43c9e5fa4a4b2f5ae102498c2c0e
Geethanjali Veetil <gtveetil@gmail.com>
2020-05-03 13:37:39 +1000

    added database sql files

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+/* Load burgers */
+INSERT INTO burgers (id, burger_name, devoured) VALUES 
+    (1, "Zinger", false), 
+    (2, "Mighty Angus", false), 
+    (3, "Filet-o-Fish", false), 
+    (4, "Bacon Cheeseburger", false), 
+    (5, "Double Quarter Pounder with Cheese", false);
+
