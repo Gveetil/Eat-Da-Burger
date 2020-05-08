1f380186185bc8abd58288937e78e7b0d50d66d5
Geethanjali Veetil <gtveetil@gmail.com>
2020-05-08 20:40:10 +1000

    updated seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,9 +2,9 @@ USE burgers_db;
 
 /* Load burgers */
 INSERT INTO burgers (id, burger_name, devoured) VALUES 
-    (1, "Zinger", false), 
-    (2, "Mighty Angus", false), 
-    (3, "Filet-o-Fish", false), 
+    (1, "Zinger", true), 
+    (2, "Mighty Angus", true), 
+    (3, "Filet-o-Fish", true), 
     (4, "Bacon Cheeseburger", false), 
     (5, "Double Quarter Pounder with Cheese", false);
 
